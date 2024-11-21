4b3daa587f8d1b63266013442da9495a7c865bcc
Thiago Martinez <thiagomartinez1240@gmail.com>
2024-11-21 13:30:54 -0300

    El robot cumple todas las funciones del proyecto, falta pulir cosas

--- tasks.py
+++ tasks.py
@@ -4,22 +4,25 @@ from robocorp.tasks import task
 from robocorp import browser
 from RPA.HTTP import HTTP
 from RPA.Tables import Tables
+from RPA.PDF import PDF
+from RPA.Archive import Archive
 
 
 @task
-def order_robots_from_RobotSpareBin():
+def order_robots_from_robot_spare_bin():
     """Orders robots from RobotSpareBin Industries Inc.
     Saves the order HTML receipt as a PDF file.
     Saves the screenshot of the ordered robot.
     Embeds the screenshot of the robot to the PDF receipt.
     Creates ZIP archive of the receipts and the images."""
     browser.configure(slowmo=100)
-    go_to_robot_order_website()
+    open_robot_order_website()
     get_orders()
     enter_robot_data()
+    archive_receipts()
 
 
-def go_to_robot_order_website():
+def open_robot_order_website():
     """Va a la URL"""
     browser.goto(url="https://robotsparebinindustries.com/#/robot-order")
 
@@ -40,10 +43,8 @@ def get_orders():
 def enter_robot_data():
     """Carga la info hardcodeada de un robot"""
     headers = ["Order number", "Head", "Body", "Legs", "Address"]
-    # orders = get_orders()
     tables = Tables()
     parsed_orders_to_table = tables.read_table_from_csv("orders.csv", columns=headers)
-    #make_order(parsed_orders_to_table[0])
     for row in parsed_orders_to_table:
         make_order(row)
 
@@ -51,12 +52,48 @@ def enter_robot_data():
 def make_order(row):
     """Creo la orden"""
     press_ok()
-    print("FILA2", row)
+    order_number = str(row["Order number"])
     page = browser.page()
-    page.select_option("#head", str(row['Head']))
+    page.select_option("#head", str(row["Head"]))
     page.click(f"#id-body-{str(row['Body'])}")
-    page.fill('input.form-control[type="number"]', row['Legs'])
-    page.fill("#address", row['Address'])
-    page.click("#order")
+    page.fill('input.form-control[type="number"]', row["Legs"])
+    page.fill("#address", row["Address"])
+    while page.is_visible("#order"):
+        page.click("#order")
+    path_receipt = store_receipt_as_pdf(order_number)
+    path_image_preview = screenshot_robot(order_number)
+    embed_screenshot_to_receipt(path_image_preview, path_receipt, order_number)
     page.click("#order-another")
-    a='a'
+
+
+def store_receipt_as_pdf(order_number):
+    """Guarda recibo como PDF"""
+    page = browser.page()
+    pdf = PDF()
+    receipt = page.locator("#receipt").inner_html()
+    path_receipt = f"output/tmp/receipt-order-{order_number}.pdf"
+    pdf.html_to_pdf(receipt, path_receipt)
+    return path_receipt
+
+
+def screenshot_robot(order_number):
+    """Saca screenshot del robot"""
+    page = browser.page()
+    path_image_preview = f"output/tmp/robot_preview-order-{order_number}.png"
+    page.locator("#robot-preview-image").screenshot(path=path_image_preview)
+    return path_image_preview
+
+
+def embed_screenshot_to_receipt(screenshot, pdf_file, order_number):
+    """Une el pdf y la imagen en un mismo pdf"""
+    pdf = PDF()
+    pdf.add_files_to_pdf(
+        files=[pdf_file, screenshot],
+        target_document=f"output/full-orders/full-order-{order_number}.pdf",
+    )
+
+
+def archive_receipts():
+    """Convierto la carpeta donde estan las ordenes en un zip"""
+    lib = Archive()
+    lib.archive_folder_with_zip("output/full-orders", "output/full-orders/all-complete-orders.zip")
